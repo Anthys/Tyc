751886dd6149e878764e7ee2a3b0af0bda1a8c41
Anthys <julbarres@gmail.com>
2019-06-16 15:36:14 +0200

    upd

--- nearis.lua
+++ nearis.lua
@@ -6,15 +6,18 @@
 t=0
 tt=0
 indx=1
+anims=1
 game={
   n=false,
-  nc=true,
+  nc=true, --cooldown
+  ac=false, --cooldown
   s=2,
   lvl=1,
 }
 p={
   x=0,
-  y=0
+  y=0,
+  flp=0
 }
 cam={
   x=0,
@@ -33,6 +36,10 @@ m={
 }
 ents={}
 
+
+AN={
+  SW={256, 257, 258, 259, 260, 261}
+}
 --For each level, tells wich rectangles are allowed (al) numbered from L->R U->D
 LVL={
   {al={0, 1, 2, 8, 9, 16, 17, 24}}
@@ -111,6 +118,8 @@ end
 
 function level()
   if tt==0 then init_lvl() end
+  prx = p.x + 120
+  pry = p.y + 64
   cls()
   update_camera()
   update_player()
@@ -121,22 +130,74 @@ function level()
   --cls()
   update_ents()
   check_input()
+  if game.n then update_sight() end
+end
+
+function update_sight()
+  local tx, ty = (120-cam.x + m.x)//8, (64-cam.y + m.y)//8
+  local tile = mget(tx, ty)
+  if tile==38 or tile==44 then print("WOW") rectb(cam.x-120+tx*8, cam.y-64+ty*8, 18, 10, 12) end
+  if game.ac and m.l then
+    if tile == 38 then mset(tx, ty, 44) mset(tx+1, ty, 45)
+    elseif tile == 44 then mset(tx, ty, 38) mset(tx+1, ty, 39)
+    elseif tile == 241 then firestart(tx, ty) end
+    game.ac = false
+  end
+  if not m.l then game.ac = true end
 end
 
 function update_player()
-  if btn(0) then p.y=p.y-1 end
-  if btn(1) then p.y=p.y+1 end
-  if btn(2) then p.x=p.x-1 end
-  if btn(3) then p.x=p.x+1 end
+  flp = btn(3) and 0 or btn(2) and 1 or flp
+  if btn(0) then try_move(0, -1) end
+  if btn(1) then try_move(0, 1) end
+  if btn(2) then try_move(-1, 0) end
+  if btn(3) then try_move(1, 0) end
    
-  spr(p.spr,p.x+cam.x,p.y+cam.y, 0)
+  spr(p.spr,p.x+cam.x,p.y+cam.y, 0, 1, flp)
+end
+
+function try_move(x, y)
+  local yesyes=true
+  for yx=((prx+x-2)//8), ((prx+x)//8)+1 do
+    for yy=((pry+y-2)//8), ((pry+y)//8)+1 do
+    if is_full(yx, yy) then yesyes=false end
+    end
+  end
+  if yesyes then p.x=p.x+x p.y=p.y+y end
+end
+
+OVRR={[1]=true, [44]=false, [45]=false}
+
+function is_full(x, y)
+  local tile = mget(x,y)
+  if OVRR[tile]~=nil then return OVRR[tile] end
+  if tile>=38 and tile<240 then return true else return false end
 end
 
 yes= true
 function check_input()
-  if m.r and game.nc then for i=0,15 do negate(i) end game.n=not game.n game.nc=false cam.x = 0 end
+  if m.r and game.nc then for i=0,15 do negate(i) end game.n=not game.n game.nc=false end
   if not m.r then game.nc =true end
-  if not game.n and m.l then yes=false else yes=true end
+  if not game.n and m.l and anims==0 then anims=1 end
+  if anims>0 then anim_s() end
+end
+
+function anim_s()
+  if anims==7 then anims=0 end
+  local tx,ty= m.x-p.x-cam.x, m.y-p.y-cam.y
+  local tl = math.sqrt(tx*tx+ty*ty)
+  tx, ty = tx/tl, ty/tl
+  local rtt = 0
+  local xa = math.acos(tx)*180/math.pi
+  local ya = math.asin(ty)*180/math.pi, 0, 10
+  xa= ya>0 and -xa or xa
+  xa = xa//90
+  if xa==0 then rtt=0 elseif xa==-1 then rtt=1 elseif xa==-2 then rtt=2 else rtt=3 end
+  print(xa)
+  print(ya, 0 ,10)
+  tx,ty=(tx*15 +p.x+cam.x), (ty*15+p.y+cam.y)
+  spr(AN.SW[anims], tx,ty, 0, 1, 0, rtt)
+  if t%2==0 then anims=anims+1 end
 end
 
 function block_cam(x, y)
@@ -145,42 +206,67 @@ end
 
 
 function update_camera()
-  prx = p.x + 120
-  pry = p.y + 64
-  --[[local clean = true
+  local yx, yy = 0, 0
+  local clean = true
+  --[[
   for x=prx//8-cam.lx//8//2,prx//8+cam.lx//8//2 do
-    trace("--")
+    for y = pry//8, pry//8+1 do
+    if block_cam(x, y) then yx=x yy=y cam.x,cam.y = lerp(cam.x,120+(100-(-p.x-120+x*8))-p.x, 0.05), cam.y clean = true end
+    end
+  end
+  for y=pry//8-cam.ly//8//2,pry//8+cam.ly//8//2 do
+    for x = prx//8, prx//8+1 do
+      if block_cam(x, y) then yx=x yy=y cam.x,cam.y = cam.x, lerp(cam.y,64+(50-(-p.y-64+y*8))-p.y, 0.05) clean = true end
+    end
+  end
+  --]]
+  --[[
+  for x=prx//8-cam.lx//8//2,prx//8+cam.lx//8//2 do
+    trace("--")]]--
     print(cam.x, 0, 30)
     print(240-cam.x)
     print(p.x + 120, 0, 10)
-    trace(p.x-cam.x+120)
-    trace((p.x+cam.x)//8)
+    --trace(p.x-cam.x+120)
+    --trace((p.x+cam.x)//8)
+    --[[
     for y=pry//8-cam.ly//8//2,pry//8+cam.ly//8//2 do
     --for a,y in pairs({pry//8-cam.ly//8//2, pry//8+cam.ly//8//2}) do
-      if block_cam(x, y) then cam.x,cam.y = lerp(cam.x,p.x-120, 0.05), cam.y clean = true break end
+      if block_cam(x, y) then yx=x yy=y cam.x,cam.y = lerp(cam.x,120+(100-(-p.x-120+x*8))-p.x, 0.05), lerp(cam.y,64+(50-(-p.y-64+y*8))-p.y, 0.05) clean = true break end
     end
   end
+  ]]--
   --for y=pry//8-cam.ly//8//2,pry//8+cam.ly//8//2 do
   --  for a,x in pairs({prx//8-cam.lx//8//2, prx//8+cam.lx//8//2}) do
   --    if block_cam(x, y) then clean = false break end
   --  end
   --end
   print(clean, 0, 20)
-  ]]--
+  
   if true then
   cam.x=math.min(120,lerp(cam.x,120-p.x,0.05))
   cam.y=math.min(64,lerp(cam.y,64-p.y,0.05))
+  --if math.abs(240-cam.x-(120+p.x))<0.5 then cam.x = round(cam.x) end
+  --if math.abs(128-cam.y-(64+p.y))<0.5 then cam.y = round(cam.y) end
   dx,dy=cam.x-120,cam.y-64
   cam.cx=cam.x/8+(cam.x%8==0 and 1 or 0)
   cam.cy=cam.y/8+(cam.y%8==0 and 1 or 0) end
   map(15-cam.cx,8-cam.cy,31,18,(cam.x%8)-8,(cam.y%8)-8,0)
+  --rect(cam.x-120+yx*8, cam.y-64+yy*8, 2, 2, 12)
+end
+
+function round(num, numDecimalPlaces)
+  local mult = 10^(numDecimalPlaces or 0)
+  return math.floor(num * mult + 0.5) / mult
 end
 
 function update_ents()
   for k,l in pairs(ents.spawn) do
-    spr(0, cam.x-120+l[1]*8, cam.y-64+l[2]*8) end
-  for i,j in pairs(ents) do
-    if i~="spawn" then spr(1, j[1]*8-cam.x, j[2]*8-cam.y) print(j[1]*8-cam.x, 10, 10) end 
+    if not game.n then spr(0, cam.x-120+l[1]*8, cam.y-64+l[2]*8) end end
+  for a,b in pairs(ents) do
+    if a~="spawn" and a~="fireplaces" then 
+      for i,j in pairs(b) do
+      spr(j[3], cam.x-120+j[1], cam.y-64+j[2], 0) print(j[1], 10, 10) end 
+      end
   end
 end
 
@@ -193,15 +279,42 @@ end
 function init_lvl()
   p={x=0,y=0,spr=496}
   ents.spawn={}
+  ents.fireplaces={}
   for i,j in pairs(LVL[game.lvl].al) do
     for x=j%8*30,j%8*30+30 do
       for y=j//8*17,j//8*17+17 do
-        if mget(x, y) == 240 then table.insert(ents.spawn,{x,y,0}) end
+        local tile = mget(x,y)
+        if tile == 240 or tile==241 then table.insert(ents.spawn,{x,y,0}) end
+        if tile == 241 then make_places(x,y) end
       end
     end
   end
 end
 
+function firestart(x, y)
+  local di = ents.fireplaces[x*100+y]
+  trace("--")
+  if not di[1] then
+    di[1]=true
+    ents[x*100+y] = {}
+  for i,j in pairs(di) do
+    if i~=1 then
+    trace(j[1])
+    table.insert(ents[x*100+y], {j[1]*8,j[2]*8, 265})
+    end
+  end
+else ents[x*100+y]=nil di[1]=false
+end
+end
+
+function make_places(x,y)
+  ents.fireplaces[x*100+y]={false}
+  for i=x-3,x+3 do
+    for j=y+1,y+4 do
+        if mget(i, j) == 7 then table.insert(ents.fireplaces[x*100+y], {i,j}) end
+    end
+  end
+end
 
 function pal(i,r,g,b)
 	--sanity checks
