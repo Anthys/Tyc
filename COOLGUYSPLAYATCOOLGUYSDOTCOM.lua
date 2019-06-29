c494bb8932604a0a16d9755f3379a8c4d5d2df7f
JBarres <julbarres@gmail.com>
2019-06-29 14:52:23 +0200

    yes

--- COOLGUYSPLAYATCOOLGUYSDOTCOM.lua
+++ COOLGUYSPLAYATCOOLGUYSDOTCOM.lua
@@ -2,8 +2,14 @@ function TIC()
   --put fonction here
 end
 
+
+-- Cool concepts
 function bugged_text_but_lisible(arrayoftext)
   cls()
   local t_txt = arrayoftext[t//90 + 1]
   print(string.sub(t_txt, 1, math.min(t//5%20, string.len(t_txt))))
 end
+
+
+--Utilities
+function lerp(a,b,t) return (1-t)*a + t*b end
