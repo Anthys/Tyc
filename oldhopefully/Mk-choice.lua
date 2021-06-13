7505edbdcf0eea780b072a1c97732829b74e930e
Anthys <julbarres@gmail.com>
2021-06-13 17:15:09 +0200

    t

---
+++ oldhopefully/Mk-choice.lua
@@ -0,0 +1,427 @@
+A={
+  [1]={""},
+  [2]={{{"Oh..."}, ["s"]= 15},["t"]={50}},
+  [3]={{"Hi...", ["s"]= 15}, ["t"]={50}},
+  [4]={"You're lucky, I almost missed your call...",["t"]={50}},
+  [5]={"Who knows what would've happened if I had...",["t"]={50}},
+  [6]={{"So.", ["s"]= 8}, ["t"]={50}},
+  [7]={"Discreet", ["t"]={20}},
+  [8]={"Let's see if I can help you with that.", ['t']={50}},
+  [9]={"INITIALIZING INPUT"},
+  [10]={"Here.", ["t"]={50}},
+  [11]={{{"I think that's slighly better,","don't you agree?"}},{"Yes"}, {"No", 13}},
+  [12]={"Cool.",["t"]={50, 14}},
+  [13]={"Don't be fussy, I'm only doing that for you.", ["t"]={50}},
+  [14]={"Right.",["t"]={50}},
+  [15]={{"So, how do you feel right now?"}, {"Fine."}, {{"I'm not feeling"," anything."}, 17}},
+  [16]={"Nice to hear that.", ["t"]={50, 18}},
+  [17]={"Understandable.", ["t"]={50}},
+  [18]={{{"I will have to ask you a few questions,", "if you don't mind."}}, ["t"]={50}},
+  [19]={"That's just the protocol.", ["t"]={50}},
+  [20]={"What is your name?", {{"I do not ","remember."}, 999}, {"Candace.", ["r"]="My name is Candace."}},
+  --CandaceRoute
+  [21]={"Oh, how funny, that's mine too!",["t"]={50}},
+  [22]={"Nice to meet you, Candace.", ["t"]={50}},
+  [23]={{{"So, do you have any memory"," about the incident?"}} ,{"Which incident?"}, {"I think I remember.", ["r"]={"I remember blades. And pain.", "A lot of pain."}}},
+  [30]={{{"Do you remember any visual element, ", "if this doesn't sounds too impolite?"}}, {{"I think I will stop", "talking about it."}}},
+  [31]={"I'm sorry. Please accept my apologies, I didn't want to hurt you."},
+  [32]={{{"Let's switch to a lighter topic.", "Do you have any family, or relatives that we could contact?"}}}
+}
+DEF_WAIT = 60
+DEF_TXT_S = 3
+ST={"intro"}
+SRY={"X", "X", "X", "Wrong way.", "Sorry.", "Not at all.", "I don't think so.", "Please try again.", "Damn, no.", "There are at most four directions.", "This is beginning to be kind of weird.", "I'm sorry, I'm not interested.", "You don't seem to be the smarter, huh?"}
+
+g={
+  s=1,
+  z=false --Allow use of z to hurt the IA implanted in your head
+}
+t, tt = 0, 0
+t_compt_wrong=1
+tcompt=0
+wait_t=0
+can_pass=true
+
+q={
+  --[[states: 
+  0 is black screen between questions
+  1 is question appearing
+  2 is choices appearing
+  3 is waiting for input
+  4 is answer appearing
+  5 is remain time
+  ]]--
+  state=0,
+  t=0
+}
+
+zc=0
+cur_i = 1
+cur_q = A[cur_i]
+cur_ans = nil
+
+function TIC()
+  _G[ST[g.s]]()
+  t=t+1
+  tt=tt+1
+end
+
+function dc(object)
+  local lookup_table = {}
+  local function _copy(object)
+      if type(object) ~= "table" then
+          return object
+      elseif lookup_table[object] then
+          return lookup_table[object]
+      end
+      local new_table = {}
+      lookup_table[object] = new_table
+      for index, value in pairs(object) do
+          new_table[_copy(index)] = _copy(value)
+      end
+      return setmetatable(new_table, getmetatable(object))
+  end
+  return _copy(object)
+end
+
+function lpx(txt)
+  return print(txt, -100, -100)
+end
+
+function make_answer(txt)
+  if type(txt)=="table" then for i,v in pairs(yes) do
+    print(v, 120-lpx(txt)//2, 120-#txt*9+i*9-9) end
+  else print(txt, 120-lpx(txt)//2, 120) end
+end
+
+function show_choices()
+  for i,j in pairs(A[cur_i]) do
+    if i~=1 then
+      if i==2 then 
+        if type(j[1])=="table" then for k,l in pairs(j[1]) do
+          print(l, 0, 120-#j*9+k*9-9) end
+        else print(j[1], 0, 120) end --line(20, 50, 20, 80, 15)
+      elseif i==3 then
+        if type(j[1])=="table" then for k,l in pairs(j[1]) do
+          print(l, 240-print(l, -100, -100), 120-#j*9+k*9-9) end
+        else print(j[1], 240-print(j[1], -100, -100), 120) end -- line(225, 50, 225, 80, 15)
+      elseif i==4 then print(j[1], 120-(print(j[1], -100, -100)//2), 128) 
+      elseif i==5 then print(j[1], 120-(print(j[1], -100, -100)//2), 2) end
+    end
+  end
+  for i=2,3 do
+    if btnp(i) then
+      if A[cur_i][i] and A[cur_i][i][2] then movnext(A[cur_i][i][2])
+      elseif A[cur_i][i] then movnext(cur_i+1) end
+    end
+  end
+  for i=0,1 do
+    if btnp(i) and A[cur_i][5-i] and A[cur_i][5-i][2] then movnext(A[cur_i][5-i][2]) end
+  end
+end
+
+function movnext(indx, ans)
+  ans = ans or ""
+  if ans=="" then nexq(indx)
+  else return 3 end
+end
+
+
+function nexq(indx)
+  tt = 0
+  wait_t=0
+  can_pass=false
+  cur_ans = nil
+  if A[indx] then
+    cur_i = indx
+    cur_q = dc(A[cur_i])
+    if cur_i==-1 then cur_q[1]=SRY[t_compt_wrong] t_compt_wrong=t_compt_wrong+1 end
+  end
+end
+
+function show_quest()
+  txt = cur_q[1]
+  if cur_i==1 then
+    if btnp(1) and tt//60>2 then
+      ccount=1
+      tt = 0
+      tcompt=tcompt+1
+      if tcompt>2 then nexq(2) end
+    elseif tt//60>1 then
+      m_arrow()
+    end
+    return
+  elseif cur_i==7 then
+    local txt="I see that you're quite... discrete."
+    print(string.sub(txt,0,math.min(tt//DEF_TXT_S, string.len(txt)-10)), 120-print(txt, -100, -100)//2, 64)
+    if tt//DEF_TXT_S>string.len(txt)-10+30 then
+      print(string.sub(txt,0,math.min(tt//2-string.len(txt)+10-30, string.len(txt))), 120-print(txt, -100, -100)//2, 64)
+    end
+    if tt//DEF_TXT_S>string.len(txt)-10+30+30 then nexq(8) end
+    return
+  elseif cur_i==9 then
+    if tt//20<20 and tt//20%6>1 then
+      spr(1, math.min(3, -30+tt/8), 64, 0, 1, 0, 1, 2, 2)
+      spr(1, math.max(219, 254-tt/8), 57, 0, 1, 0, 3, 2, 2) 
+    elseif tt//20>20 then
+      nexq(10)
+    end
+  end
+  if cur_q["t"] and can_pass then 
+    if cur_q["t"][1]+DEF_REMAIN>0 then cur_q["t"][1]=cur_q["t"][1]-1 
+    else 
+      if #cur_q["t"]>1 then nexq(cur_q["t"][2]) else nexq(cur_i + 1) end
+    end
+  end
+  printtxt(txt)
+end
+
+txt_t=0
+
+function printtxt(txt)
+  if type(txt)=="string" then
+    if false then 
+      print(txt, 120-lpx(txt)//2, 64)
+    else 
+      can_pass = math.min(tt//DEF_TXT_S, string.len(txt))==string.len(txt)
+      print(string.sub(txt,0,math.min(tt//DEF_TXT_S, string.len(txt))), 120-lpx(txt)//2, 64)
+    end
+  elseif type(txt)=="table" then
+    local a = txt["s"] or DEF_TXT_S
+    local txt = txt[1]
+    if type(txt)=="table" then
+      local waiter = 0
+      for i,j in ipairs(txt) do
+        print(can_pass)
+        --print(print(txt[2], -100, -100), 0, 30)
+        --print(tt//a-waiter, 0, i*10)
+        if (tt//a-waiter)>=0 then
+          print(string.sub(j,0,math.min(tt//a-waiter, string.len(j))), 120-lpx(j)//2, 64-#txt*9+i*9)
+        end
+        waiter=waiter+string.len(j)
+      end
+      can_pass = tt//a-waiter>=0
+    else
+      local j = txt
+      print(string.sub(txt,0,math.min(tt//a, string.len(j))), 120-lpx(txt)//2, 64)
+      can_pass=tt//a-string.len(j)>=0
+    end
+  end
+end 
+
+function check_input()
+  if g.z and btnp(4) then return end--make_screen_white_and_then_fade_to_previous_color
+end
+
+function white_to_black(i, c)
+  local r,g,b = peek(0x3fc0+(i*3)),peek(0x3fc0+(i*3)+1),peek(0x3fc0+(i*3)+2)
+  if c<=30 then r,g,b=255,255,255 c=c+1
+  elseif r<=0 then r,g,b=0,0,0 c=0
+  else r,g,b=r-1,g-1,b-1 c=c+1
+  end
+  poke(0x3fc0+(i*3)+2,b)
+	poke(0x3fc0+(i*3)+1,g)
+  poke(0x3fc0+(i*3),r)
+  return c
+end
+
+function m_arrow()
+  if tt//5%20<10 then
+    spr(1, 116, 110 + tt//5%10, 0, 1, 0, 0, 2, 2)
+  else spr(1, 116, 110 + 10 - tt//5%10, 0, 1, 0, 0, 2, 2)
+  end
+end
+
+ccount = 0
+DEF_REMAIN = 60
+backcol=0
+
+function intro()
+  --cls(backcol)
+  --if wait_t<DEF_WAIT and wait_t>-1 then wait_t=wait_t+1 return elseif wait_t>-1 then tt=0 wait_t=-1 end
+  --show_choices()
+  --show_quest()
+  --if ccount~=0 then ccount=white_to_black(0,ccount) end
+  state_q()
+end
+
+function state_q()
+  q.t=q.t+1
+  if q.state==0 then
+    cls(backcol)
+    if q.t>DEF_WAIT then q.state=1 q.t=0 end
+  elseif q.state==1 then
+    cls(backcol)
+    if render_quest(q.t) then q.state=2 q.t=0 end
+  elseif q.state==2 then
+    cls(backcol)
+    render_quest(0, true)
+    --local temp = show_choices()
+    if q.t>0 then q.state=3 q.t=0 end --0 is default waiting for input
+  elseif q.state==3 then
+    cls(backcol)
+    render_quest(0, true)
+    local temp = show_choices2() or time_flow()
+    if temp then thechosen=temp q.state=4 q.t=0 finwait = false end
+  elseif q.state==4 then
+    cls(backcol)
+    if not thechosen then print("ERROR") return end
+    if not thechosen["r"] then q.state=5 q.t=0 return
+    else
+      if finwait and q.t < 60 then show_ans(thechosen["r"], 1000) return 
+      elseif finwait then nexq2(thechosen[1]) q.state=0 q.t=0 end
+      if show_ans(thechosen["r"], q.t) then finwait=true q.t=0 end --maybe add remain time in nexq
+    end
+  elseif q.state==5 then nexq2(thechosen[1]) q.state=0 q.t=0
+  end
+  if thechosen then print(thechosen["r"], 0, 10) end
+  print(q.state)
+end
+
+function time_flow()
+  print(q.t, 0, 30)
+  if cur_q["t"] and q.t>cur_q["t"][1] then
+    output = {}
+    if cur_q["t"][2] then output[1]=cur_q['t'][2] else output[1]=cur_i+1 end
+    if cur_q["t"][3] then output["r"]=cur_q["t"][3] end
+    return output
+  end
+end
+
+function show_ans(msg, time)
+  local a = DEF_TXT_S
+  txt = msg
+  if type(msg)=="string" then
+    ended = math.min(time//a, string.len(txt))==string.len(txt)
+    print(string.sub(txt,0,math.min(time//a, string.len(txt))), 120-lpx(txt)//2, 130)
+  elseif type(msg)=="table" then
+    local waiter=0
+    for i,v in pairs(msg) do
+      if (time//a-waiter)>=0 then
+        print(string.sub(v,0,math.min(time//a-waiter, string.len(v))), 120-lpx(v)//2, 130-#msg*9+i*9)
+      end
+      waiter=waiter+string.len(v)
+    end
+  ended = time//a-waiter>=0
+  end
+  return ended
+end
+
+thechosen = nil --Type: {INDX TO GO, ["r"]=ANSW}
+
+function movnext2(n_and_i)
+  if cur_q[n_and_i[2]]["r"] then thechosen=cur_q[n_and_i[2]]["r"] 
+  else thechosen=nil end
+end
+
+
+function nexq2(indx)
+  tt = 0
+  wait_t=0
+  can_pass=false
+  cur_ans = nil
+  if A[indx] then
+    cur_i = indx
+    cur_q = dc(A[cur_i])
+    if cur_i==-1 then cur_q[1]=SRY[t_compt_wrong] t_compt_wrong=t_compt_wrong+1 end
+  end
+end
+
+function show_choices2()
+  for i,j in pairs(A[cur_i]) do
+    if i~=1 then
+      if i==2 then 
+        if type(j[1])=="table" then for k,l in pairs(j[1]) do
+          print(l, 0, 120-#j*9+k*9) end
+        else print(j[1], 0, 120) end --line(20, 50, 20, 80, 15)
+      elseif i==3 then
+        if type(j[1])=="table" then for k,l in pairs(j[1]) do
+          print(l, 240-print(l, -100, -100), 120-#j*9+k*9) end
+        else print(j[1], 240-print(j[1], -100, -100), 120) end -- line(225, 50, 225, 80, 15)
+      elseif i==4 then print(j[1], 120-(print(j[1], -100, -100)//2), 128) 
+      elseif i==5 then print(j[1], 120-(print(j[1], -100, -100)//2), 2) end
+    end
+  end
+  for i=2,3 do
+    if btnp(i) then
+      if A[cur_i][i] and A[cur_i][i][2] then return {A[cur_i][i][2], ["r"]=get_ans(i)}
+      elseif A[cur_i][i] then return {cur_i+1, ["r"]=get_ans(i)} end
+    end
+  end
+  for i=0,1 do
+    if btnp(i) and A[cur_i][5-i] and A[cur_i][5-i][2] then return {A[cur_i][5-i][2], ["r"]=get_ans(5-i)} end
+  end
+end
+
+function get_ans(iofcurq)
+  trace(iofcurq)
+  trace(#cur_q)
+  if cur_q[iofcurq]["r"] then return cur_q[iofcurq]["r"] end
+end
+
+function render_quest(time, showfull)
+  if showfull==true then time=9999999 end
+  txt = cur_q[1]
+  if cur_i==1 then
+    if btnp(1) and tt//60>2 then
+      ccount=1
+      tt = 0
+      tcompt=tcompt+1
+      if tcompt>2 then nexq2(2) end
+    elseif tt//60>1 then
+      m_arrow()
+    end
+    return
+  elseif cur_i==7 then
+    local txt="I see that you're quite... discrete."
+    print(string.sub(txt,0,math.min(tt//DEF_TXT_S, string.len(txt)-10)), 120-print(txt, -100, -100)//2, 64)
+    if tt//DEF_TXT_S>string.len(txt)-10+30 then
+      print(string.sub(txt,0,math.min(tt//2-string.len(txt)+10-30, string.len(txt))), 120-print(txt, -100, -100)//2, 64)
+    end
+    if tt//DEF_TXT_S>string.len(txt)-10+30+30 then nexq2(8) end
+    return
+  elseif cur_i==9 then
+    if tt//20<20 and tt//20%6>1 then
+      spr(1, math.min(3, -30+tt/8), 64, 0, 1, 0, 1, 2, 2)
+      spr(1, math.max(219, 254-tt/8), 57, 0, 1, 0, 3, 2, 2) 
+    elseif tt//20>20 then
+      nexq2(10)
+    end
+  end
+  return printtxt2(txt, time)
+end
+
+function printtxt2(txt, time)
+  local ended = true
+  if type(txt)=="string" then
+    if false then 
+      print(txt, 120-lpx(txt)//2, 64)
+    else 
+      ended = math.min(time//DEF_TXT_S, string.len(txt))==string.len(txt)
+      print(string.sub(txt,0,math.min(time//DEF_TXT_S, string.len(txt))), 120-lpx(txt)//2, 64)
+    end
+  elseif type(txt)=="table" then
+    local a = txt["s"] or DEF_TXT_S
+    local txt = txt[1]
+    if type(txt)=="table" then
+      local waiter = 0
+      for i,j in ipairs(txt) do
+        --print(can_pass)
+        --print(print(txt[2], -100, -100), 0, 30)
+        --print(tt//a-waiter, 0, i*10)
+        if (time//a-waiter)>=0 then
+          print(string.sub(j,0,math.min(time//a-waiter, string.len(j))), 120-lpx(j)//2, 64-#txt*9+i*9)
+        end
+        waiter=waiter+string.len(j)
+      end
+    ended = time//a-waiter>=0
+    else
+      local j = txt
+      print(string.sub(txt,0,math.min(time//a, string.len(j))), 120-lpx(txt)//2, 64)
+      ended=time//a-string.len(j)>=0
+    end
+  end
+  return ended
+end 
+
+for i,j in ipairs({[1]=1, [2]=2, [4]=4}) do trace(i) trace(j) end trace("--")
---
+++ oldhopefully/nearis.lua
@@ -0,0 +1,359 @@
+-- title:  game title
+-- author: game developer
+-- desc:   short description
+-- script: lua
+-- input:  mouse
+t=0
+tt=0
+indx=1
+anims=1
+game={
+  n=false,
+  nc=true, --cooldown
+  ac=false, --cooldown
+  s=2,
+  lvl=1,
+}
+p={
+  x=0,
+  y=0,
+  flp=0
+}
+cam={
+  x=0,
+  y=0,
+  cx=0,
+  cy=0,
+  lx=120,
+  ly=120
+}
+m={
+  x=0,
+  y=0,
+  l=0,
+  m=0,
+  r=0,
+}
+ents={}
+
+
+AN={
+  SW={256, 257, 258, 259, 260, 261}
+}
+--For each level, tells wich rectangles are allowed (al) numbered from L->R U->D
+LVL={
+  {al={0, 1, 2, 8, 9, 16, 17, 24}}
+}
+ST={"intro", "level"}
+STR={
+  INTRO={
+    "Note to myself:",
+    "I've been trapped in a time loop.",
+    "And I escaped it.",
+    "...            ",
+    "I hope.           "
+  },
+  THING1={
+    "The person I'm closest is a vampire.",
+    "And with this statement coexists the fact that vampire don't exist.",
+    "",
+    "When did my life stopped making any sense?"
+  },
+  THING2={
+    "Am I...",
+    "",
+    "Am I still in the loop?",
+  },
+  THING3={
+    "My head is spinning.",
+    "How am I still in it?",
+    "HOW IS IT POSSIBLE?"
+  }
+}
+
+NTS={
+  THING1="As crazy as it may seem, I believe he's trying to escape.",
+  THING2="Stability is near breackdown. Just a little more effort."
+}
+
+function TIC()
+  _G[ST[game.s]]()
+  t=t+1
+  tt=tt+1
+end
+
+function intro()
+  if wr_messages(STR.INTRO) then
+    game.s = game.s+1
+  return
+  end
+end
+
+--NOT USED
+function contains(tab, val)
+  for index, value in ipairs(tab) do
+      if value==val then
+          return true
+      end
+  end
+  return false
+end
+
+function wr_messages(arr)
+  cls()
+  if tt//8>string.len(arr[indx]) then
+    indx=indx+1
+    tt=0
+    if indx>#arr then
+      indx = 0
+      return true
+    end
+  end
+  local t_txt=arr[indx]
+  local color = 12
+  print(string.sub(t_txt, 1, math.min(tt//5, string.len(t_txt))), 0, 0, color)
+  print(indx, 10, 10)
+  return false
+end
+
+function level()
+  if tt==0 then init_lvl() end
+  prx = p.x + 120
+  pry = p.y + 64
+  cls()
+  update_camera()
+  hider()
+  update_player()
+  rect(cam.x-120+8,cam.y-64+8,8,8,2)
+  m.x, m.y, m.l, m.m, m.r = mouse()
+  --
+  --if tt%60*2==0 then spawn_ents() end 
+  --cls()
+  update_ents()
+  check_input()
+  if game.n then update_sight() end
+end
+
+function update_sight()
+  local tx, ty = (120-cam.x + m.x)//8, (64-cam.y + m.y)//8
+  local tile = mget(tx, ty)
+  if tile==38 or tile==44 then print("WOW") rectb(cam.x-120+tx*8, cam.y-64+ty*8, 18, 10, 12) end
+  if game.ac and m.l then
+    if tile == 38 then mset(tx, ty, 44) mset(tx+1, ty, 45)
+    elseif tile == 44 then mset(tx, ty, 38) mset(tx+1, ty, 39)
+    elseif tile == 241 then firestart(tx, ty) 
+    elseif tile == 84 then ents.tempor[tx*100+ty] = {60,{tx*8+1, ty*8-14, 281}, {tx*8+1, ty*8-6, 297}} end
+    game.ac = false
+  end
+  if not m.l then game.ac = true end
+end
+
+function update_player()
+  flp = btn(3) and 0 or btn(2) and 1 or flp
+  if btn(0) then try_move(0, -1) end
+  if btn(1) then try_move(0, 1) end
+  if btn(2) then try_move(-1, 0) end
+  if btn(3) then try_move(1, 0) end
+   
+  spr(p.spr,p.x+cam.x,p.y+cam.y, 0, 1, flp)
+end
+
+function try_move(x, y)
+  local yesyes=true
+  if p.x+120+x<0 or p.y+64+y<0 then return end
+  for yx=((prx+x-2)//8), ((prx+x)//8)+1 do
+    for yy=((pry+y-2)//8), ((pry+y)//8)+1 do
+    if is_full(yx, yy) then yesyes=false end
+    end
+  end
+  if yesyes then p.x=p.x+x p.y=p.y+y end
+end
+
+OVRR={[1]=true, [44]=false, [45]=false}
+
+function is_full(x, y)
+  local tile = mget(x,y)
+  if OVRR[tile]~=nil then return OVRR[tile] end
+  if tile>=38 and tile<240 then return true else return false end
+end
+
+yes= true
+function check_input()
+  if m.r and game.nc then for i=0,15 do negate(i) end game.n=not game.n game.nc=false end
+  if not m.r then game.nc =true end
+  if not game.n and m.l and anims==0 then anims=1 end
+  if anims>0 then anim_s() end
+end
+
+function anim_s()
+  if anims==7 then anims=0 end
+  local tx,ty= m.x-p.x-cam.x, m.y-p.y-cam.y
+  local tl = math.sqrt(tx*tx+ty*ty)
+  tx, ty = tx/tl, ty/tl
+  local rtt = 0
+  local xa = math.acos(tx)*180/math.pi
+  local ya = math.asin(ty)*180/math.pi, 0, 10
+  xa= ya>0 and -xa or xa
+  xa = xa//90
+  if xa==0 then rtt=0 elseif xa==-1 then rtt=1 elseif xa==-2 then rtt=2 else rtt=3 end
+  print(xa)
+  print(ya, 0 ,10)
+  tx,ty=(tx*15 +p.x+cam.x), (ty*15+p.y+cam.y)
+  spr(AN.SW[anims], tx,ty, 0, 1, 0, rtt)
+  if t%2==0 then anims=anims+1 end
+end
+
+function block_cam(x, y)
+  if mget(x,y) == 1 then return true else return false end
+end
+
+
+function update_camera()
+  local yx, yy = 0, 0
+  local clean = true
+  --[[
+  for x=prx//8-cam.lx//8//2,prx//8+cam.lx//8//2 do
+    for y = pry//8, pry//8+1 do
+    if block_cam(x, y) then yx=x yy=y cam.x,cam.y = lerp(cam.x,120+(100-(-p.x-120+x*8))-p.x, 0.05), cam.y clean = true end
+    end
+  end
+  for y=pry//8-cam.ly//8//2,pry//8+cam.ly//8//2 do
+    for x = prx//8, prx//8+1 do
+      if block_cam(x, y) then yx=x yy=y cam.x,cam.y = cam.x, lerp(cam.y,64+(50-(-p.y-64+y*8))-p.y, 0.05) clean = true end
+    end
+  end
+  --]]
+  --[[
+  for x=prx//8-cam.lx//8//2,prx//8+cam.lx//8//2 do
+    trace("--")]]--
+    print(cam.x, 0, 30)
+    print(240-cam.x)
+    print(p.x + 120, 0, 10)
+    --trace(p.x-cam.x+120)
+    --trace((p.x+cam.x)//8)
+    --[[
+    for y=pry//8-cam.ly//8//2,pry//8+cam.ly//8//2 do
+    --for a,y in pairs({pry//8-cam.ly//8//2, pry//8+cam.ly//8//2}) do
+      if block_cam(x, y) then yx=x yy=y cam.x,cam.y = lerp(cam.x,120+(100-(-p.x-120+x*8))-p.x, 0.05), lerp(cam.y,64+(50-(-p.y-64+y*8))-p.y, 0.05) clean = true break end
+    end
+  end
+  ]]--
+  --for y=pry//8-cam.ly//8//2,pry//8+cam.ly//8//2 do
+  --  for a,x in pairs({prx//8-cam.lx//8//2, prx//8+cam.lx//8//2}) do
+  --    if block_cam(x, y) then clean = false break end
+  --  end
+  --end
+  print(clean, 0, 20)
+  
+  if true then
+  cam.x=math.min(120,lerp(cam.x,120-p.x,0.05))
+  cam.y=math.min(64,lerp(cam.y,64-p.y,0.05))
+  --if math.abs(240-cam.x-(120+p.x))<0.5 then cam.x = round(cam.x) end
+  --if math.abs(128-cam.y-(64+p.y))<0.5 then cam.y = round(cam.y) end
+  dx,dy=cam.x-120,cam.y-64
+  cam.cx=cam.x/8+(cam.x%8==0 and 1 or 0)
+  cam.cy=cam.y/8+(cam.y%8==0 and 1 or 0) end
+  map(15-cam.cx,8-cam.cy,31,18,(cam.x%8)-8,(cam.y%8)-8,0,1)
+  --rect(cam.x-120+yx*8, cam.y-64+yy*8, 2, 2, 12)
+end
+
+function round(num, numDecimalPlaces)
+  local mult = 10^(numDecimalPlaces or 0)
+  return math.floor(num * mult + 0.5) / mult
+end
+
+function hider()
+  for k,l in pairs(ents.spawn) do
+    if not game.n then spr(0, cam.x-120+l[1]*8, cam.y-64+l[2]*8) end end
+end
+
+function update_ents()
+  for a,b in pairs(ents) do
+    if a~="spawn" and a~="fireplaces" then 
+      if a=="tempor" then
+        for i,j in pairs(b) do
+          if j[1]<0 then ents.tempor[i]=nil else j[1]=j[1]-1 for k,l in pairs(j) do if k~=1 then spr(l[3], cam.x-120+l[1], cam.y-64+l[2], 0) end end end
+        end
+      else
+      for i,j in pairs(b) do
+      spr(j[3], cam.x-120+j[1], cam.y-64+j[2], 0) print(j[1], 10, 10) 
+      end end 
+    end
+  end
+end
+
+function spawn_ents()
+  for i,j in pairs(ents.spawn) do
+    if j[3]~=1 then j[3]=1 table.insert(ents,{j[1],j[2],50}) end
+  end
+end
+
+function init_lvl()
+  p={x=0,y=0,spr=496}
+  ents.spawn={}
+  ents.fireplaces={}
+  ents.tempor={}
+  for i,j in pairs(LVL[game.lvl].al) do
+    for x=j%8*30,j%8*30+30 do
+      for y=j//8*17,j//8*17+17 do
+        local tile = mget(x,y)
+        if tile == 240 or tile==241 then table.insert(ents.spawn,{x,y,0}) end
+        if tile == 241 then make_places(x,y) end
+      end
+    end
+  end
+end
+
+function firestart(x, y)
+  local di = ents.fireplaces[x*100+y]
+  trace("--")
+  if not di[1] then
+    di[1]=true
+    ents[x*100+y] = {}
+  for i,j in pairs(di) do
+    if i~=1 then
+    trace(j[1])
+    table.insert(ents[x*100+y], {j[1]*8,j[2]*8, 265})
+    end
+  end
+else ents[x*100+y]=nil di[1]=false
+end
+end
+
+function make_places(x,y)
+  ents.fireplaces[x*100+y]={false}
+  for i=x-3,x+3 do
+    for j=y+1,y+4 do
+        if mget(i, j) == 7 then table.insert(ents.fireplaces[x*100+y], {i,j}) end
+    end
+  end
+end
+
+function pal(i,r,g,b)
+	--sanity checks
+	if i<0 then i=0 end
+	if i>15 then i=15 end
+	--returning color r,g,b of the color
+	if r==nil and g==nil and b==nil then
+		return peek(0x3fc0+(i*3)),peek(0x3fc0+(i*3)+1),peek(0x3fc0+(i*3)+2)
+	else
+		if r==nil or r<0 then r=0 end
+		if g==nil or g<0 then g=0 end
+		if b==nil or b<0 then b=0 end
+		if r>255 then r=255 end
+		if g>255 then g=255 end
+		if b>255 then b=255 end
+		poke(0x3fc0+(i*3)+2,b)
+		poke(0x3fc0+(i*3)+1,g)
+		poke(0x3fc0+(i*3),r)
+	end
+end
+
+function lerp(a,b,t) return (1-t)*a + t*b end
+
+function negate(i)
+  r,g,b = 255-peek(0x3fc0+(i*3)),255-peek(0x3fc0+(i*3)+1),255-peek(0x3fc0+(i*3)+2)
+  poke(0x3fc0+(i*3)+2,b)
+	poke(0x3fc0+(i*3)+1,g)
+  poke(0x3fc0+(i*3),r)
+end
